499085ee9a1eb0e1fdac53b22108cd70e1f5ba89
pkm774 <mprabhat774@gmail.com>
2023-02-24 12:50:28 +0530

    FC: Declare PID variables as constants
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- Flight Controller/YMFC-AL_Flight_controller/YMFC-AL_Flight_controller.ino
+++ Flight Controller/YMFC-AL_Flight_controller/YMFC-AL_Flight_controller.ino
@@ -18,53 +18,73 @@
 #include <Wire.h>                          //Include the Wire.h library so we can communicate with the gyro.
 #include <EEPROM.h>                        //Include the EEPROM.h library so we can store information onto the EEPROM
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////
 //PID gain and limit settings
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-float pid_p_gain_roll = 1.3;               //Gain setting for the roll P-controller
-float pid_i_gain_roll = 0.04;              //Gain setting for the roll I-controller
-float pid_d_gain_roll = 18.0;              //Gain setting for the roll D-controller
-int pid_max_roll = 400;                    //Maximum output of the PID-controller (+/-)
+/////////////////////////////////
 
-float pid_p_gain_pitch = pid_p_gain_roll;  //Gain setting for the pitch P-controller.
-float pid_i_gain_pitch = pid_i_gain_roll;  //Gain setting for the pitch I-controller.
-float pid_d_gain_pitch = pid_d_gain_roll;  //Gain setting for the pitch D-controller.
-int pid_max_pitch = pid_max_roll;          //Maximum output of the PID-controller (+/-)
+const float pid_p_gain_roll = 1.3;               //Gain setting for the roll P-controller
+const float pid_i_gain_roll = 0.00;              //Gain setting for the roll I-controller
+const float pid_d_gain_roll = 18.0;              //Gain setting for the roll D-controller
+const int pid_max_roll = 400;                    //Maximum output of the PID-controller (+/-)
 
-float pid_p_gain_yaw = 4.0;                //Gain setting for the pitch P-controller. //4.0
-float pid_i_gain_yaw = 0.02;               //Gain setting for the pitch I-controller. //0.02
-float pid_d_gain_yaw = 0.0;                //Gain setting for the pitch D-controller.
-int pid_max_yaw = 400;                     //Maximum output of the PID-controller (+/-)
+const float pid_p_gain_pitch = pid_p_gain_roll;  //Gain setting for the pitch P-controller.
+const float pid_i_gain_pitch = pid_i_gain_roll;  //Gain setting for the pitch I-controller.
+const float pid_d_gain_pitch = pid_d_gain_roll;  //Gain setting for the pitch D-controller.
+const int pid_max_pitch = pid_max_roll;          //Maximum output of the PID-controller (+/-)
 
-boolean auto_level = true;                 //Auto level on (true) or off (false)
+const float pid_p_gain_yaw = 4.0;                //Gain setting for the pitch P-controller. //4.0
+const float pid_i_gain_yaw = 0.02;               //Gain setting for the pitch I-controller. //0.02
+const float pid_d_gain_yaw = 0.0;                //Gain setting for the pitch D-controller.
+const int pid_max_yaw = 400;                     //Maximum output of the PID-controller (+/-)
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const boolean auto_level = true;                 //Auto level on (true) or off (false)
+
+///////////////////////////////
 //Declaring global variables
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-byte last_channel_1, last_channel_2, last_channel_3, last_channel_4;
+///////////////////////////////
+
+byte last_channel_1;
+byte last_channel_2;
+byte last_channel_3;
+byte last_channel_4;
 byte eeprom_data[36];
-byte highByte, lowByte;
-volatile int receiver_input_channel_1, receiver_input_channel_2, receiver_input_channel_3, receiver_input_channel_4;
-int counter_channel_1, counter_channel_2, counter_channel_3, counter_channel_4, loop_counter;
+
+volatile int receiver_input_channel_1;
+volatile int receiver_input_channel_2;
+volatile int receiver_input_channel_3;
+volatile int receiver_input_channel_4;
+
+int counter_channel_1;
+int counter_channel_2;
+int counter_channel_3;
+int counter_channel_4;
+int loop_counter;
+
 int esc_1, esc_2, esc_3, esc_4;
 int throttle, battery_voltage;
-int cal_int, start, gyro_address;
-int receiver_input[5];
-int temperature;
-int acc_axis[4], gyro_axis[4];
+int cal_int, start, gyro_address, temperature;
+int receiver_input[5], acc_axis[4], gyro_axis[4];
+
 float roll_level_adjust, pitch_level_adjust;
 
 long acc_x, acc_y, acc_z, acc_total_vector;
-unsigned long timer_channel_1, timer_channel_2, timer_channel_3, timer_channel_4, esc_timer, esc_loop_timer;
-unsigned long timer_1, timer_2, timer_3, timer_4, current_time;
-unsigned long loop_timer;
+
+unsigned long timer_channel_1, timer_1;
+unsigned long timer_channel_2, timer_2;
+unsigned long timer_channel_3, timer_3;
+unsigned long timer_channel_4, timer_4;
+unsigned long current_time, esc_timer, esc_loop_timer, loop_timer;
+
 double gyro_pitch, gyro_roll, gyro_yaw;
 double gyro_axis_cal[4];
+
 float pid_error_temp;
 float pid_i_mem_roll, pid_roll_setpoint, gyro_roll_input, pid_output_roll, pid_last_roll_d_error;
 float pid_i_mem_pitch, pid_pitch_setpoint, gyro_pitch_input, pid_output_pitch, pid_last_pitch_d_error;
 float pid_i_mem_yaw, pid_yaw_setpoint, gyro_yaw_input, pid_output_yaw, pid_last_yaw_d_error;
 float angle_roll_acc, angle_pitch_acc, angle_pitch, angle_roll;
+
 boolean gyro_angles_set;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
