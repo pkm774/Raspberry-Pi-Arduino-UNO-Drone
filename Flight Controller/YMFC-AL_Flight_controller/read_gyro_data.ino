82defea3b5ad3b44076e337c739137f4d8056fa8
pkm774 <mprabhat774@gmail.com>
2023-02-24 13:44:05 +0530

    FC: improve IMU data reading
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- Flight Controller/YMFC-AL_Flight_controller/read_gyro_data.ino
+++ Flight Controller/YMFC-AL_Flight_controller/read_gyro_data.ino
@@ -14,7 +14,7 @@ void read_gyro_data(){
     receiver_input_channel_2 = convert_receiver_channel(2);                 //Convert the actual receiver signals for roll to the standard 1000 - 2000us.
     receiver_input_channel_3 = convert_receiver_channel(3);                 //Convert the actual receiver signals for throttle to the standard 1000 - 2000us.
     receiver_input_channel_4 = convert_receiver_channel(4);                 //Convert the actual receiver signals for yaw to the standard 1000 - 2000us.
-    
+  
     while(Wire.available() < 14);                                           //Wait until the 14 bytes are received.
     acc_axis[1] = Wire.read()<<8|Wire.read();                               //Add the low and high byte to the acc_x variable.
     acc_axis[2] = Wire.read()<<8|Wire.read();                               //Add the low and high byte to the acc_y variable.
@@ -25,22 +25,27 @@ void read_gyro_data(){
     gyro_axis[3] = Wire.read()<<8|Wire.read();                              //Read high and low part of the angular data.
   }
 
-  if(cal_int == 2000){
-    gyro_axis[1] -= gyro_axis_cal[1];                                       //Only compensate after the calibration.
-    gyro_axis[2] -= gyro_axis_cal[2];                                       //Only compensate after the calibration.
-    gyro_axis[3] -= gyro_axis_cal[3];                                       //Only compensate after the calibration.
+  if(cal_int == 2000){                                                      //Only compensate after the calibration.
+    gyro_axis[1] -= gyro_axis_cal[1];
+    gyro_axis[2] -= gyro_axis_cal[2];
+    gyro_axis[3] -= gyro_axis_cal[3];
   }
+
   gyro_roll = gyro_axis[eeprom_data[28] & 0b00000011];                      //Set gyro_roll to the correct axis that was stored in the EEPROM.
-  if(eeprom_data[28] & 0b10000000)gyro_roll *= -1;                          //Invert gyro_roll if the MSB of EEPROM bit 28 is set.
+  gyro_roll = (eeprom_data[28] & 0b10000000) ? -gyro_roll : gyro_roll;      //Invert gyro_roll if the MSB of EEPROM bit 28 is set.
+
   gyro_pitch = gyro_axis[eeprom_data[29] & 0b00000011];                     //Set gyro_pitch to the correct axis that was stored in the EEPROM.
-  if(eeprom_data[29] & 0b10000000)gyro_pitch *= -1;                         //Invert gyro_pitch if the MSB of EEPROM bit 29 is set.
+  gyro_pitch = (eeprom_data[29] & 0b10000000) ? -gyro_pitch : gyro_pitch;   //Invert gyro_pitch if the MSB of EEPROM bit 29 is set.
+
   gyro_yaw = gyro_axis[eeprom_data[30] & 0b00000011];                       //Set gyro_yaw to the correct axis that was stored in the EEPROM.
-  if(eeprom_data[30] & 0b10000000)gyro_yaw *= -1;                           //Invert gyro_yaw if the MSB of EEPROM bit 30 is set.
+  gyro_yaw = (eeprom_data[30] & 0b10000000) ? -gyro_yaw : gyro_yaw;         //Invert gyro_yaw if the MSB of EEPROM bit 30 is set.
 
   acc_x = acc_axis[eeprom_data[29] & 0b00000011];                           //Set acc_x to the correct axis that was stored in the EEPROM.
-  if(eeprom_data[29] & 0b10000000)acc_x *= -1;                              //Invert acc_x if the MSB of EEPROM bit 29 is set.
+  acc_x = (eeprom_data[29] & 0b10000000) ? -acc_x : acc_x;                  //Invert acc_x if the MSB of EEPROM bit 29 is set.
+
   acc_y = acc_axis[eeprom_data[28] & 0b00000011];                           //Set acc_y to the correct axis that was stored in the EEPROM.
-  if(eeprom_data[28] & 0b10000000)acc_y *= -1;                              //Invert acc_y if the MSB of EEPROM bit 28 is set.
+  acc_y = (eeprom_data[28] & 0b10000000) ? -acc_y : acc_y;                  //Invert acc_y if the MSB of EEPROM bit 28 is set.
+
   acc_z = acc_axis[eeprom_data[30] & 0b00000011];                           //Set acc_z to the correct axis that was stored in the EEPROM.
-  if(eeprom_data[30] & 0b10000000)acc_z *= -1;                              //Invert acc_z if the MSB of EEPROM bit 30 is set.
+  acc_z = (eeprom_data[30] & 0b10000000) ? -acc_z : acc_z;                  //Invert acc_z if the MSB of EEPROM bit 30 is set.
 }
